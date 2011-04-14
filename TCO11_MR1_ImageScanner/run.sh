75a3b9037dfe7e04b031433e873b5c6e8b49a000
Erwan Demairy <Erwan.Demairy@inria.fr>
2011-04-14 16:50:58 +0200

    - removal of built files. - addition of a launching script (run.sh).

---
+++ TCO11_MR1_ImageScanner/run.sh
@@ -0,0 +1,3 @@
+export MR1_ROOT=${HOME}/Documents/Personal/TC/TCO11_MR1_ImageScanner/
+cd ${MR1_ROOT}/files
+java -jar ImageScannerVis.jar -exec "java -jar ${MR1_ROOT}/dist/TCO11_MR1_ImageScanner.jar"
